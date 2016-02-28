d39c3b9af0fc041e89d843adfd78583852592cde
Andrey Mokrousov <insanedeveloper@ya.ru>
2016-02-28 20:43:11 +0300

    wip

--- tests/9.whitespace.test.js
+++ tests/9.whitespace.test.js
@@ -1,52 +1,105 @@
-(function(views) {
-    'use strict';
-
-    crazy: try {
-        bla > bal | ee;
-        if (!erp) {
-            here: while (!qwerty) {
-
-                continue here;
-            }
-            do {
-            } while (true);
-        } else {
-            lalala;
-        }
-        switch (waf) {
-            case 'lala':
-                break;
-            case 'wtf':
-            default:
-                groove();
-                return -1;
-        }
-        for (var i = 0; i < qqq; i++) {
-            here: beAmused();
-            continue;
-        }
-        for (i = 0; i < qqq; i++) {
-            here: beAmusedAgain();
-            continue;
-        }
-        for (;;)
-            nothingToDo();
-        for(allGood in world)
-            for(var rabbits in hole)
-                cry();
-        var e = [
-            2, 234, 234, 234
-        ].map(function(item) {
-            return -item;
-        });
-        var t = b = ert === false + ( 'all good'? null : undefined);
-        var uberExpression = (true || false) && (typeof null);
-        var t = 4 >>> 5 | !2;
-      
-        t = 1,2,3,4,5;
-    } catch (e) {
-        throw new Error(e);
-    } finally {
-        beGood();
+/* literals */
+1;
+1.45;
+2e-45;
+true;
+false;
+null;
+undefined;
+'hello world';
+[1, 2, 3];
+/* var declaration */
+var a = 1;
+var b = true,
+    object = {
+        good: true,
+        reason: undefined
+    },
+    c = function() {};
+object.x = 11;
+for (var i = 0; i < 10; i ++) ;
+for (var key in object) ;
+/* functions */
+function bla(){}
+function blabla(param){}
+function blablable(param, param2){}
+function blablable   (param, param2)  {
+    return {
+        x: true
+    };
+}
+(function invokeMe(global){
+    if (!global.batman) {
+        throw new Evil();
     }
-})(views);
+    return global.state;
+})(this);
+
+var x = function (param) {},
+    y = function bla(param1, param2) {
+    return 0 + arguments.length;
+};
+x = y('x', 'y');
+/* loops */
+do {
+    ;
+} while (false);
+do    {}    while (false)  ;
+
+while(false) {}
+while(false){};
+while (false){  ;  }
+useless: while(false){
+    continue useless;
+}
+for (var i = 0;;){}
+for(a = 0;;){}
+for (;;)    {}
+for (var i = 0; i < 0; i++) {}
+for (a = 0; a < 10; a++)
+    ;
+for(var key in object)
+    ;
+/* conditionals */
+if (a >= b) a = b;
+if (b < c) { ; } else 42;
+if (b > c) {
+    b++;
+}else{  }
+b = a ? e : t;
+b = a? e:t;
+b = a?e:t;
+b = w || (a = x);
+switch(true) {
+    case true:
+        b = 42;
+        break;
+    case a === 4 | 5:
+        b = 41;
+
+    default:
+        b = 0;
+
+}
+/* try */
+try {
+    endThisMess();
+}catch(qwe){}
+try{endThisMess();}catch(qwe){;}
+
+function world() {
+    try {
+        toBeGood();
+    } catch (evil) {
+        delete evil.root;
+    } finally {
+        return new Hope();
+    }
+}
+/* operator precedence */
+a = (2 + 5) * (3 - 2);
+b = 2 + 5 * 3 - 2;
+c = 2 * 5 + 3 * 2;
+d = 2 * 5 + 3;
+e = 2 + 5 * 3;
+s = 5 + 4 + 3 + 2;
